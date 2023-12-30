f5f17c22fe370528a3f2e2286a6339d9a074e6cd
Felix Li <04felix.li@gmail.com>
2023-12-30 00:00:51 -0500

    Added more properties to ProjectObject interface. Changed techStack property to array and use .map() to render tech stack inside of Project.tsx

--- src/Components/Projects/Project.tsx
+++ src/Components/Projects/Project.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../../GlobalStyles.css";
 import { ProjectObject } from "../../Util/ProjectObjects";
 import { Link } from "react-router-dom";
+import { textChangeRangeNewSpan } from "typescript";
 
 interface Props {
     project: ProjectObject
@@ -15,11 +16,16 @@ const Project = ({ project }: Props) => {
                 <img src={project.imagePath} alt={`${project.name}Image`} className={`w-full h-full border border-color6 border-4 rounded-lg ${project.name !== 'Portfolio' ? 'object-cover' : 'object-contain'}`} />
                 <div className="absolute -bottom-16 -right-24 rounded-lg bg-color6 w-11/12 h-4/6 text-sm p-6 z-5 shadow-md text-left flex flex-col justify-between">
                     <p>
-                        {project.description}
+                        {project.shortDescription}
                     </p>
-                    <p>
-                        {project.techStack}
-                    </p>
+                    <section className="flex flex-row flex-wrap">
+                        {project.techStack.map((tech: string, index: number) => (
+                            <React.Fragment key={tech}>
+                                <p>{tech}</p>
+                                {index !== project.techStack.length - 1 && <p>&nbsp;|&nbsp;</p>}
+                            </React.Fragment>
+                        ))}
+                    </section>
                 </div>
             </button>
         </Link>
--- src/Util/ProjectObjects.tsx
+++ src/Util/ProjectObjects.tsx
@@ -1,31 +1,55 @@
 export interface ProjectObject {
     name: string;
-    description: string;
-    techStack: string;
+    shortDescription: string;
+    techStack: string[];
     imagePath: string;
     URL: string;
+    overview: string;
+    features: string;
+    otherInfo: string;
+    githubLink: string | null;
+    githubLinkFE: string | null;
+    githubLinkBE: string | null;
 }
 
 export const IMFATProject: ProjectObject = {
     name: 'IMFAT',
-    description: 'IMFAT is an Individually Managed Fitness Activity Tracker, a comprehensive web application empowering gym-goers to track their workouts and personal metrics, monitor progress, and create customized workout templates effortlessly.',
-    techStack: 'React | Node.js | Express.js | Typescript | PostgreSQL | Supabase | Tailwind CSS',
+    shortDescription: 'IMFAT is an Individually Managed Fitness Activity Tracker, a comprehensive web application empowering gym-goers to track their workouts and personal metrics, monitor progress, and create customized workout templates effortlessly.',
+    techStack: ['React', 'Node.js', 'Express.js', 'Typescript', 'PostgreSQL', 'Supabase', 'Tailwind CSS'],
     imagePath: '/Images/WorkoutHistory.png',
-    URL: "/Projects/IMFAT"
+    URL: "/Projects/IMFAT",
+    overview: '',
+    features: '',
+    otherInfo: '',
+    githubLink: null,
+    githubLinkFE: '',
+    githubLinkBE: ''
 }
 
 export const ChessProject: ProjectObject = {
     name: 'PvP Chess',
-    description: 'A two player chess game with features such as legal move validation and checkmate/stalemate checking.',
-    techStack: 'Python | Pygame',
+    shortDescription: 'A two player chess game with features such as legal move validation and checkmate/stalemate checking.',
+    techStack: ['Python', 'Pygame'],
     imagePath: '/Images/ChessGame.png',
-    URL: "/Projects/Chess"
+    URL: "/Projects/Chess",
+    overview: '',
+    features: '',
+    otherInfo: '',
+    githubLink: '',
+    githubLinkFE: null,
+    githubLinkBE: null
 }
 
 export const PortfolioProject: ProjectObject = {
     name: 'Portfolio',
-    description: 'A personal website used to show others stuff about my life, my personal projects, and work experience.',
-    techStack: 'React | TypeScript | Tailwind CSS',
+    shortDescription: 'A personal website used to show others stuff about my life, my personal projects, and work experience.',
+    techStack: ['React', 'Typescript', 'Tailwind CSS'],
     imagePath: '/Images/PersonalPortfolio.png',
-    URL: "/Projects/Portfolio"
+    URL: "/Projects/Portfolio",
+    overview: '',
+    features: '',
+    otherInfo: '',
+    githubLink: '',
+    githubLinkFE: '',
+    githubLinkBE: ''
 }
