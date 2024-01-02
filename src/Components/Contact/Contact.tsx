8d3c37ab870a93b74596c99332ab13482b510ddf
Felix Li <04felix.li@gmail.com>
2024-01-01 21:48:18 -0500

    Working on contact card functionality

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,36 +1,47 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import emailjs from '@emailjs/browser';
 
-interface FormData {
-    email: string;
-    name: string;
-    message: string;
-}
 
-const Contact: React.FC = () => {
-    const [formData, setFormData] = useState<FormData>({
-        email: '',
-        name: '',
-        message: ''
-    });
+const Contact = () => {
+    const form = useRef<HTMLFormElement>();
+    const [buttonText, setButtonText] = useState<string>("Send message");
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { id, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [id]: value
-        }));
-    };
+    useEffect(() => {
+        const button = document.getElementById('sendButton');
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        if (button) {
+            const buttonRect = button.getBoundingClientRect();
+            console.log('Button width:', buttonRect.width, 'px');
+            console.log('Button height:', buttonRect.height, 'px');
+        }
+    }, [])
+
+    const sendEmail = async (e: React.FormEvent) => {
         e.preventDefault();
-        // Handle form submission here (you can use formData state)
-        console.log('Form submitted:', formData);
-        // Reset form after submission
-        setFormData({
-            email: '',
-            name: '',
-            message: ''
-        });
+
+        if (form.current) {
+            setButtonText("Sending...");
+            try {
+
+                await emailjs.sendForm(
+                    process.env.REACT_APP_EMAILJS_SERVICE_ID!,
+                    process.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
+                    form.current,
+                    process.env.REACT_APP_EMAILJS_PUBLIC_KEY!
+                );
+
+                setButtonText("Sent");
+
+                setTimeout(() => {
+                    setButtonText("Send message");
+                    form.current?.reset(); // Reset the form fields
+                }, 1000); // Revert button text after 1 second
+
+            } catch (error) {
+                console.error('Error sending email:', error);
+
+            }
+        }
     };
 
     return (
@@ -45,40 +56,46 @@ const Contact: React.FC = () => {
 
                 <section className="bg-color4 ml-36 w-5/12 border border-4 border-color6 rounded-lg">
                     <div className="p-6 max-w-screen-md">
-                        <form onSubmit={handleSubmit} className="space-y-8">
+                        <form ref={form as React.RefObject<HTMLFormElement>} onSubmit={sendEmail} className="space-y-8">
                             <input
                                 type="text"
                                 id="Name"
-                                value={formData.name}
-                                onChange={handleChange}
+                                // value={formData.name}
+                                // onChange={handleChange}
                                 className="shadow-sm bg-gray-50 border border-color4 text-sm rounded-lg block w-full p-2.5 focus:outline-none focus:ring-0 focus:shadow-lg"
-                                placeholder="Name"
-                            // required
+                                placeholder="Name*"
+                                name="user_name"
+                                required
                             />
                             <input
                                 type="email"
                                 id="email"
-                                value={formData.email}
-                                onChange={handleChange}
+                                // value={formData.email}
+                                // onChange={handleChange}
                                 className="shadow-sm bg-gray-50 border border-color4 text-sm rounded-lg block w-full p-2.5 focus:outline-none focus:ring-0 focus:shadow-lg"
-                                placeholder="Email"
-                            // required
+                                placeholder="Email*"
+                                name="user_email"
+                                required
                             />
                             <div className="sm:col-span-2">
                                 <textarea
                                     id="message"
                                     rows={6}
-                                    value={formData.message}
-                                    onChange={handleChange}
+                                    // value={formData.message}
+                                    // onChange={handleChange}
                                     className="shadow-sm bg-gray-50 border border-color4 text-sm rounded-lg block w-full p-2.5 focus:outline-none focus:ring-0 focus:shadow-lg"
-                                    placeholder="Message"
+                                    placeholder="Message*"
+                                    name="message"
+                                    required
                                 ></textarea>
                             </div>
                             <button
                                 type="submit"
-                                className="py-3 px-5 text-sm text-center rounded-full bg-color6 focus:ring-0 focus:outline-none transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:shadow-lg duration-300"
+                                id="sendButton"
+                                className={`py-3 px-5 text-sm text-center rounded-full bg-color6 focus:ring-0 focus:outline-none transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:shadow-lg duration-300 w-32 ${buttonText === "Sent" ? 'bg-green-300' : ''}`}
+                                value="Send"
                             >
-                                Send message
+                                {buttonText}
                             </button>
                         </form>
                     </div>
